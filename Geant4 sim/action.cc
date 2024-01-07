c9e3eaf13234c854372b9c090a0808d47295f8f1
Ferro-J <146795973+Ferro-J@users.noreply.github.com>
2024-01-08 00:01:54 +0100

    Update action.cc
    Accidentally left comments in from old files. Removing them for a few

--- Geant4 sim/action.cc
+++ Geant4 sim/action.cc
@@ -10,10 +10,4 @@ void MyActionInitialization::Build() const
 {
     MyPrimaryGenerator *generator = new MyPrimaryGenerator();
     SetUserAction(generator);
-
-    //MyRunAction *runAction = new MyRunAction(); NEED TO RESTORE THE RUN.CC AND RUN.HH FILES AS SEEN INPHYSICS MATTERS TUTORIAL
-    //SetUserAction(runAction);
-
-    //MySteppingAction *steppingAction = new MySteppingAction();
-    //SetUserAction(steppingAction);
 }
